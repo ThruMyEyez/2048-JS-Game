c9de543d0cebb35c4dfe25380880964eea4d4b19
Artur Baumeister <Artur.baumeister@gmail.com>
2022-07-16 15:24:13 +0200

    Add setter value(v)

--- tile.js
+++ tile.js
@@ -2,12 +2,22 @@ export default class Tile {
   #tileElement
   #x
   #y
-
+  #value
+    //creating random tiles numbers with 50% chance for a 2 or a 4.
   constructor(tileContainer, value = Math.random() > .5 ? 2 : 4) {
     this.#tileElement = document.createElement("div");
     this.#tileElement.classList.add("tile");
     tileContainer.append(this.#tileElement);
     this.value = value;
+  }
+
+  set value(v) {
+    this.#value = v;
+    this.#tileElement.textContent = v;
+    const power = Math.log2(v);
+    const backgroundLightness = 100 - power * 9; //if power gets bigger decrease bg-lightness
+    this.#tileElement.style.setProperty("--background-lightness", `${backgroundLightness}%`);
+    this.#tileElement.style.setProperty("--text-lightness", `${backgroundLightness <= 50 ? 90 : 10}%`) //if bg lightness is less or equal to 50 use a bright color.
   }
 
   set x(value) {
